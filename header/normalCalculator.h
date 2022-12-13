5073ce7a83cb999784be9b37a0f0785a2bf19f7d
ShamanOfCode <evgeniymarkov2015@gmail.com>
2022-12-13 21:59:45 +0100

    moving headers to header/

---
+++ header/normalCalculator.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef NORMALCALCULATOR_H
+#define NORMALCALCULATOR_H
+#include <iostream>
+#include <math.h>
+
+namespace nC {
+	class calculation{
+
+	};
+	calculation calcO;
+}
+
+#endif // NORMALCALCULATOR_H
